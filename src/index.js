c8bdd07a910e472dc963a6f1d483acf97cf2fd71
xiayichen <602001624@qq.com>
2020-02-16 17:55:59 +0800

    基本页面及外层路由

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
 import { render } from "react-dom";
 import React from "react";
 import App from "./App";
-render(<App />, document.querySelector("#root"));
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import { mainRouter } from "./routes";
+
+render(
+  <Router>
+    <Switch>
+      <Route
+        path="/admin"
+        render={routerProps => {
+          //todo 权限:需要登录才能访问/admin
+          return <App {...routerProps} />;
+        }}
+      />
+      {mainRouter.map(route => {
+        return (
+          <Route
+            key={route.pathname}
+            path={route.pathname}
+            component={route.component}
+          />
+        );
+      })}
+      <Redirect to="/admin" from="/" exact />
+      <Redirect to="/404" />
+    </Switch>
+  </Router>,
+  document.querySelector("#root")
+);
---
+++ src/views/index.js
@@ -0,0 +1,8 @@
+import Dashboard from "./Dashboard/Dashboard";
+import Login from "./Login/Login";
+import NotFound from "./NotFound/NotFound";
+import Settings from "./Settings/Setting";
+import ArticleList from "./Article/ArticleList";
+import ArticleEdit from "./Article/Edit";
+
+export { Dashboard, Login, NotFound, Settings, ArticleList, ArticleEdit };
